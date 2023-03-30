6d72081b361ed82bdce24f031d4f6c914364002f
somramnani <som.ramnani@gmail.com>
2023-03-30 15:44:42 -0400

    set up input states with the buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import Searchbar from "./components/Searchbar";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const handleOnChange = (event) => {
+    event.preventDefault();
+    let value = event.target.value;
+    setInput(value);
+    console.log(value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(input);
+  };
+
   return (
     <div className="App">
-      <Searchbar />
+      <Searchbar
+        input={input}
+        handleOnChange={handleOnChange}
+        handleOnSubmit={handleOnSubmit}
+      />
     </div>
   );
 }

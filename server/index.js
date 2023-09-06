28b1f6f4e1c6ae91c18809c94c0dabab93f3e7b1
somramnani <som.ramnani@gmail.com>
2023-09-05 21:35:49 -0400

    update server, add api call & update onClick

--- server/index.js
+++ server/index.js
@@ -10,8 +10,22 @@ app.get("/", (req, res) => {
   res.send("Welcome to server");
 });
 
-app.get("/cors/:dat", async (req, res) => {
+app.get("/cors/:data", async (req, res) => {
   //api request
+  await axios({
+    method: "get",
+    url: `https://api.yelp.com/v3/businesses/search?location=${req.params.data}&categories=restaurants&limit=50`,
+    headers: {
+      Authorization:
+        "Bearer t915wybDO3WP4B26tfMQd3AfvQQNFnODtP3EvVzYdhWW58RNwjSwA6OEt2mFI4zaWOwMFHgEr9Pzf3sXFiR_VooC8dfCEBqMR2MtifV8r-JEgaDpoOdQ9RcRE4sjXXYx",
+    },
+  })
+    .then((response) => {
+      return res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //have the server listen
